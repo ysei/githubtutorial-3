8c68b65785d2440f88003e90beca0feace727bc5
John Doe <johndoe@example.com>
2013-10-19 14:51:56 +0530

    basic structure ready

--- index.py
+++ index.py
@@ -1,2 +1,14 @@
-a=input('enter the number')
-print a
+def menu():
+    pass
+def cube_volume():
+    pass
+def cube_surface_area():
+    pass
+def cuboid_volume():
+    pass
+def cuboid_surface_area():
+    pass
+def cuboid_volume():
+    pass
+def cuboid_surface_area():
+    pass
