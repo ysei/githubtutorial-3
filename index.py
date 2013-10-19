b4e39e938d38fd040760a4234d1289868477ee19
John Doe <johndoe@example.com>
2013-10-19 15:58:16 +0530

    new commit

--- index.py
+++ index.py
@@ -14,5 +14,14 @@ def cuboid_surface_area():
     pass
 
 
+def menu():
+    print('enter')
+    print('1 for cube volume')
+    print('2 for cube surface area')
+    print('3 for cuboid volume ')
+    print('4 for cuboid surface area')
+    print('5 to exit')
+    a=input('')
+    
 
-
+menu()
